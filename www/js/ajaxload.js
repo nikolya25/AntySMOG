4df2b130af738ad3ef6449dc0c544cb39260975a
Nikolya <kolya_shchur@mail.ru>
2017-05-09 17:00:47 +0200

    whitelist test

---
+++ www/js/ajaxload.js
@@ -0,0 +1,13 @@
+$.mobile.allowCrossDomainPages = true;
+$.support.cors = true;
+$.mobile.phonegapNavigationEnabled = true
+
+$.ajax({
+	method: "GET",
+	//url: "http://api.gios.gov.pl/pjp-api/rest/data/getData/2745",
+	url: "http://api.gios.gov.pl/pjp-api/rest/station/findAll",
+	dataType: "json"
+}).done(function (data) {
+    document.getElementById('test').innerHTML = data[5].stationName;
+	console.log(data);
+});
