9f71f96e22f75fee700101adcfa045b42d0c181c
siekiera92 <piotr.siekierski.92@gmail.com>
2017-05-10 18:31:43 +0200

    trzecia stacja pomiarowa

--- www/js/ajaxload.js
+++ www/js/ajaxload.js
@@ -1,5 +1,8 @@
 var normaPM10 = 50;
 var normaNO2 = 200;
+var normaCO = 10000;
+var normaBenzen = 5;
+var normaPM25 = 25;
 $.ajax({
 	method: "GET",
 	url: "http://api.gios.gov.pl/pjp-api/rest/station/findAll",
@@ -76,6 +79,8 @@ $.ajax({
 
 });
 
+// ładowanie danych ze stacji 1
+
 function station1dane(){
 	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/16786", function( dane1 ) {
 			for (var x = 0; x < 24; x++) {
@@ -95,6 +100,7 @@ function station1dane(){
 			})
 }
 
+// ładowanie danych ze stacji 2
 
 function station2dane(){
 	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/16377", function( dane1 ) {
@@ -130,6 +136,88 @@ function station2dane(){
 			})
 }
 
+// ładowanie danych ze stacji 3
+
+function station3dane(){
+	//pm10
+	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/2750", function( dane1 ) {
+			for (var x = 0; x < 24; x++) {
+			
+			    if (dane1.values[x].value != null) {
+			    	document.getElementById('station3PM10').innerHTML = dane1.values[x].value;
+			    	document.getElementById('station3PM10Date').innerHTML = dane1.values[x].date;
+			    	normPM10Now=(dane1.values[x].value / normaPM10)*100;
+			    	document.getElementById('station3PM10Norm').innerHTML = normPM10Now
+			    	break;
+			    }
+			}
+			
+	});
+	//NO2
+	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/2747", function( dane1 ) {
+			for (var x = 0; x < 24; x++) {
+			
+			    if (dane1.values[x].value != null) {
+			    	document.getElementById('station3NO2').innerHTML = dane1.values[x].value;
+			    	document.getElementById('station3NO2Date').innerHTML = dane1.values[x].date;
+			    	norm3NO2Now=(dane1.values[x].value / normaNO2)*100;
+			    	document.getElementById('station3NO2Norm').innerHTML = norm3NO2Now
+			    	break;
+			    }
+			}
+			
+	});
+	//CO
+	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/2745", function( dane1 ) {
+			for (var x = 0; x < 24; x++) {
+			
+			    if (dane1.values[x].value != null) {
+			    	document.getElementById('station3CO').innerHTML = dane1.values[x].value;
+			    	document.getElementById('station3CODate').innerHTML = dane1.values[x].date;
+			    	normCONow=(dane1.values[x].value / normaCO)*100;
+			    	document.getElementById('station3CONorm').innerHTML = normCONow
+			    	break;
+			    }
+			}
+			
+	});
+	//Benzen
+	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/16500", function( dane1 ) {
+			for (var x = 0; x < 24; x++) {
+			
+			    if (dane1.values[x].value != null) {
+			    	document.getElementById('station3Benzen').innerHTML = dane1.values[x].value;
+			    	document.getElementById('station3BenzenDate').innerHTML = dane1.values[x].date;
+			    	normBenzenNow=(dane1.values[x].value / normaBenzen)*100;
+			    	document.getElementById('station3BenzenNorm').innerHTML = normBenzenNow
+			    	break;
+			    }
+			}
+			
+	});
+	//PM25
+	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/data/getData/2752", function( dane1 ) {
+			for (var x = 0; x < 24; x++) {
+			
+			    if (dane1.values[x].value != null) {
+			    	document.getElementById('station3PM25').innerHTML = dane1.values[x].value;
+			    	document.getElementById('station3PM25Date').innerHTML = dane1.values[x].date;
+			    	normPM25Now=(dane1.values[x].value / normaPM25)*100;
+			    	document.getElementById('station3PM25Norm').innerHTML = normPM25Now
+			    	break;
+			    }
+			}
+			
+	});
+	$.getJSON( "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/400", function( stan ) {
+			  document.getElementById('station3PM10Stan').innerHTML = stan.pm10IndexLevel.indexLevelName;
+			  document.getElementById('station3NO2Stan').innerHTML = stan.no2IndexLevel.indexLevelName;
+			  document.getElementById('station3COStan').innerHTML = stan.coIndexLevel.indexLevelName;
+			  document.getElementById('station3BenzenStan').innerHTML = stan.c6h6IndexLevel.indexLevelName;
+			  document.getElementById('station3PM25Stan').innerHTML = stan.pm25IndexLevel.indexLevelName;
+
+			})
+}
 
 
 
