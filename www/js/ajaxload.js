76378ba8eb51d79efc5797865b365b57f84f6c69
siekiera92 <piotr.siekierski.92@gmail.com>
2017-05-10 11:00:48 +0200

    testowanie listy stacji 1.2

--- www/js/ajaxload.js
+++ www/js/ajaxload.js
@@ -4,8 +4,29 @@ $.ajax({
 	url: "http://api.gios.gov.pl/pjp-api/rest/station/findAll",
 	dataType: "json"
 }).done(function (data) {
-    document.getElementById('station1Name').innerHTML = data[71].stationName;
+
+    document.getElementById('station1Name').innerHTML = data[20].stationName;
+    document.getElementById('station1Adress').innerHTML = data[20].addressStreet;
+    document.getElementById('station2Name').innerHTML = data[22].stationName;
+    document.getElementById('station2Adress').innerHTML = data[22].addressStreet;
+    document.getElementById('station3Name').innerHTML = data[70].stationName;
+    document.getElementById('station3Adress').innerHTML = data[70].addressStreet;
+    document.getElementById('station4Name').innerHTML = data[92].stationName;
+    document.getElementById('station4Adress').innerHTML = data[92].addressStreet;
+    document.getElementById('station5Name').innerHTML = data[100].stationName;
+    document.getElementById('station5Adress').innerHTML = data[100].addressStreet;
+    document.getElementById('station6Name').innerHTML = data[123].stationName;
+    document.getElementById('station6Adress').innerHTML = data[123].addressStreet;
+    document.getElementById('station7Name').innerHTML = data[125].stationName;
+    document.getElementById('station7Adress').innerHTML = data[125].addressStreet;
+    document.getElementById('station8Name').innerHTML = data[143].stationName;
+    document.getElementById('station8Adress').innerHTML = data[143].addressStreet;
+    document.getElementById('station9Name').innerHTML = data[161].stationName;
+    document.getElementById('station9Adress').innerHTML = data[161].addressStreet;
+    
 	console.log(data);
 });
+
+
 
 
