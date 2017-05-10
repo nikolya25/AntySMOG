a8da567a06eacb3a1b011d853c6c960d2b9637ea
siekiera92 <piotr.siekierski.92@gmail.com>
2017-05-10 11:10:18 +0200

    testowanie listy stacji 1.3

--- www/js/ajaxload.js
+++ www/js/ajaxload.js
@@ -9,8 +9,8 @@ $.ajax({
     document.getElementById('station1Adress').innerHTML = data[20].addressStreet;
     document.getElementById('station2Name').innerHTML = data[22].stationName;
     document.getElementById('station2Adress').innerHTML = data[22].addressStreet;
-    document.getElementById('station3Name').innerHTML = data[70].stationName;
-    document.getElementById('station3Adress').innerHTML = data[70].addressStreet;
+    document.getElementById('station3Name').innerHTML = data[71].stationName;
+    document.getElementById('station3Adress').innerHTML = data[71].addressStreet;
     document.getElementById('station4Name').innerHTML = data[92].stationName;
     document.getElementById('station4Adress').innerHTML = data[92].addressStreet;
     document.getElementById('station5Name').innerHTML = data[100].stationName;
@@ -19,10 +19,10 @@ $.ajax({
     document.getElementById('station6Adress').innerHTML = data[123].addressStreet;
     document.getElementById('station7Name').innerHTML = data[125].stationName;
     document.getElementById('station7Adress').innerHTML = data[125].addressStreet;
-    document.getElementById('station8Name').innerHTML = data[143].stationName;
-    document.getElementById('station8Adress').innerHTML = data[143].addressStreet;
-    document.getElementById('station9Name').innerHTML = data[161].stationName;
-    document.getElementById('station9Adress').innerHTML = data[161].addressStreet;
+    document.getElementById('station8Name').innerHTML = data[142].stationName;
+    document.getElementById('station8Adress').innerHTML = data[142].addressStreet;
+    document.getElementById('station9Name').innerHTML = data[160].stationName;
+    document.getElementById('station9Adress').innerHTML = data[160].addressStreet;
     
 	console.log(data);
 });
