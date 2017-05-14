644b715b53a493f2fe83376480f45d8f5f595eb0
Nikolya <kolya_shchur@mail.ru>
2017-05-14 23:30:55 +0200

    colors on map

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -3,7 +3,6 @@ function init() {
 }
 
 var map;
-var data = [];
 function onDeviceReady() {
 	var div = document.getElementById("map");
 	map = plugin.google.maps.Map.getMap(div, {
@@ -31,50 +30,96 @@ function onDeviceReady() {
 	setupPush();
 }
 
-function stationsOnMap() {
-	data = [
+function stationsAndMeOnMap() {
+	var stla = [];
+	var myLat;
+	var myLng;
+	var stStany = [
+		document.getElementById('station1Stan').innerHTML,
+		document.getElementById('station2Stan').innerHTML,
+		document.getElementById('station3Stan').innerHTML,
+		document.getElementById('station4Stan').innerHTML,
+		document.getElementById('station5Stan').innerHTML,
+		document.getElementById('station6Stan').innerHTML,
+		document.getElementById('station7Stan').innerHTML,
+		document.getElementById('station8Stan').innerHTML,
+	]
+	var icons = [];
+	for (i = 0; i < stStany.length; i++) {
+		switch (stStany[i]) {
+		case "Bardzo dobry":
+			icons[i] = 'blue';  // green = lime
+			break;
+		case "Dobry":
+			icons[i] = 'green';
+			break;
+		case "Umiarkowany":
+			icons[i] = 'yellow';
+			break;
+		case "Dostateczny":
+			icons[i] = 'orange';
+			break;
+			case "Zły":
+			icons[i] = 'red';
+			break;
+		case "Bardzo zły":
+			icons[i] = 'purple';
+			break;
+		default: 
+			icons[i] = 'blue';
+		}
+	}
+	var data = [
 		{
 			'position': {lat: 50.081197, lng: 19.895358},
 			'title': "Kraków, ul. Złoty Róg",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station1Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[0],
+			'icon': icons[0]
 		},
 		{
 			'position': {lat: 50.057447, lng: 19.946008},
 			'title': "Kraków, ul. Dietla",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station2Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[1],
+			'icon': icons[1]
 		},
 		{
 			'position': {lat: 50.057678, lng: 19.926189},
 			'title': "Kraków, Aleja Krasińskiego",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station3Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[2],
+			'icon': icons[2]
 		},
 		{
 			'position': {lat: 50.069308, lng: 20.053492},
 			'title': "Kraków, ul. Bulwarowa",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station4Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[3],
+			'icon': icons[3]
 		},
 		{
 			'position': {lat: 50.100569, lng: 20.122561},
 			'title': "Kraków, os. Wadów",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station5Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[4],
+			'icon': icons[4]
 		},
 		{
 			'position': {lat: 50.099361, lng: 20.018317},
 			'title': "Kraków, os. Piastów",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station6Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[5],
+			'icon': icons[5]
 		},
 		{
 			'position': {lat: 50.010575, lng: 19.949189},
 			'title': "Kraków, ul. Bujaka",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station7Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[6],
+			'icon': icons[6]
 		},
 		{
 			'position': {lat: 50.0192 , lng: 20.016803},
 			'title': "Kraków, ul. Telimeny",
-			'snippet': "Stan jakości powietrza: " + document.getElementById('station8Stan').innerHTML
+			'snippet': "Stan jakości powietrza: " + stStany[7],
+			'icon': icons[7]
 		}
 	];
-
+	
 	function addMarkers(data, callback) {
 		for (var i = 0; i < data.length; i++) {
 			map.addMarker(data[i])
@@ -84,23 +129,8 @@ function stationsOnMap() {
 	addMarkers(data, function(markers) {
 		markers[markers.length - 1].showInfoWindow();
 	});
-}
 
-function myPositionAndClosestStation() {
-	var stla = [];
-	var myLat;
-	var myLng;
-	
 	var onSuccess = function(position) {
-		/*alert('Latitude: '          + position.coords.latitude          + '\n' +
-					'Longitude: '         + position.coords.longitude         + '\n' +
-					'Altitude: '          + position.coords.altitude          + '\n' +
-					'Accuracy: '          + position.coords.accuracy          + '\n' +
-					'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-					'Heading: '           + position.coords.heading           + '\n' +
-					'Speed: '             + position.coords.speed             + '\n' +
-					'Timestamp: '         + position.timestamp                + '\n');
-		*/
 		myLat = position.coords.latitude;
 		myLng = position.coords.longitude;
 
@@ -111,7 +141,14 @@ function myPositionAndClosestStation() {
 		stla.sort(function(a, b){
 			return Math.sqrt(Math.pow(myLat - a.position.lat, 2) + Math.pow(myLng - a.position.lng, 2)) - Math.sqrt(Math.pow(myLat - b.position.lat, 2) + Math.pow(myLng - b.position.lng, 2))
 		});
-		document.getElementById('test1').innerHTML = "Najbliższa stacja: " + stla[0].title;
+		document.getElementById('stanNajbliższej').innerHTML = stla[0].snippet;
+		
+		map.addMarker({
+			'position': {"lat": myLat, "lng": myLng},
+			'title': 'Tu jestem',
+			'snippet': stla[0].snippet,
+			'icon': "http://maps.google.com/mapfiles/kml/pal3/icon56.png"
+		});
 	}
 
     // onError Callback receives a PositionError object
